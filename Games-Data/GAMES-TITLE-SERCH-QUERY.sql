9982ac7aa5fe67252652ea6424207ec25a09fe26
fernando481 <115594712+fernando481@users.noreply.github.com>
2022-11-22 13:22:21 -0600

    DATA RESULTS

---
+++ Games-Data/GAMES-TITLE-SERCH-QUERY.sql
@@ -0,0 +1,1 @@
+SELECT  * FROM games WHERE name LIKE '%HALO%' ORDER BY name;
---
+++ Games-Data/SANDBOX-CRUD-OPERATIONS_2.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int32_value,double_value,string_value)VALUES(7034,3,'EL POLLO LOCO');
+
+--UPDATE sandbox SET int64_value = 854256855 WHERE Id = 72;
+
+--DELETE FROM sandbox WHERE id= 95;
+
+SELECT * FROM sandbox;
